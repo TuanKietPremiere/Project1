d251ac01848d29539935625b6ee4d9534c294f06
lqukhanh <lequockhanh2006z@gmail.com>
2025-04-08 22:09:05 +0700

    Kien

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <math.h>
+#include <cmath>
+#include <random>
 using namespace std;
 
 void merge(vector<int> &a, vector<int> aux, int l, int m, int r)
@@ -124,25 +125,30 @@ void heapSort(vector<int> &a)
     }
 }
 
-void naturalMergeSort(vector<int>& a){
+void naturalMergeSort(vector<int> &a)
+{
     int n = a.size();
-    vector <int> starts(n + 1);
+    vector<int> starts(n + 1);
     starts[0] = 0;
     int runCount = 0;
-    for(int i = 1; i <= n; i++){
-        if(i == n || a[i] < a[i - 1])
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n || a[i] < a[i - 1])
             starts[++runCount] = i;
     }
 
-    vector <int> aux(n);
-    while(runCount > 1){
+    vector<int> aux(n);
+    while (runCount > 1)
+    {
         int newRunCount = 0;
-        for(int i = 0; i < runCount - 1; i += 2){
+        for (int i = 0; i < runCount - 1; i += 2)
+        {
             merge(a, aux, starts[i], starts[i + 1] - 1, starts[i + 2] - 1);
             starts[newRunCount++] = starts[i];
         }
 
-        if(runCount % 2 == 1){
+        if (runCount % 2 == 1)
+        {
             starts[newRunCount++] = starts[runCount - 1];
         }
 
@@ -151,42 +157,51 @@ void naturalMergeSort(vector<int>& a){
     }
 }
 
-void insertionSort(vector<int>& a){
+void insertionSort(vector<int> &a)
+{
     int n = a.size();
-    for(int i = 1; i < n; i++){
+    for (int i = 1; i < n; i++)
+    {
         int v = a[i], j;
-        for(j = i; j > 0 && v < a[j - 1]; j--)
+        for (j = i; j > 0 && v < a[j - 1]; j--)
             a[j] = a[j - 1];
         a[j] = v;
     }
 }
 
-void radixSort(vector<int>& a){
+void radixSort(vector<int> &a)
+{
     int n = a.size();
-    vector <int> aux(n);
+    vector<int> aux(n);
 
     int iMax = 0;
-    for(int i = 1; i < n; i++){
-        if(a[i] > a[iMax]){
+    for (int i = 1; i < n; i++)
+    {
+        if (a[i] > a[iMax])
+        {
             iMax = i;
         }
     }
     int maxBit = (a[iMax] == 0) ? -1 : log2(a[iMax]);
-    
-    for(int i = 0; i <= maxBit; i++){
+
+    for (int i = 0; i <= maxBit; i++)
+    {
         int id = 0;
         int mask = 1 << i;
-        for(int j = 0; j < n; j++)
-            if(!(a[j] & mask)) aux[id++] = a[j];
-        for(int j = 0; j < n; j++)
-            if(a[j] & mask) aux[id++] = a[j];
+        for (int j = 0; j < n; j++)
+            if (!(a[j] & mask))
+                aux[id++] = a[j];
+        for (int j = 0; j < n; j++)
+            if (a[j] & mask)
+                aux[id++] = a[j];
         a = aux;
     }
 }
 
-vector<int> generateRandomSortedArray(const vector<int>& rdArr){
+vector<int> generateRandomSortedArray(const vector<int> &rdArr)
+{
     int n = rdArr.size();
-    vector <int> rdsArr(n);
+    vector<int> rdsArr(n);
     rdsArr = rdArr;
     mergeSort(rdsArr);
     return rdsArr;
@@ -279,6 +294,92 @@ vector<int> generateReverseSortedArray(int n, int k)
         swap(arr[i], arr[n - 1 - i]);
     }
     return arr;
+}
+
+void shellSort(vector<int> v)
+{
+    int n = v.size();
+
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+
+        for (int i = gap; i < n; ++i)
+        {
+            int temp = v[i];
+            int j = i;
+
+            while (j >= gap && v[j - gap] > temp)
+            {
+                v[j] = v[j - gap];
+                j -= gap;
+            }
+
+            v[j] = temp;
+        }
+    }
+}
+
+void bubbleSort(vector<int> v)
+{
+    int n = v.size();
+    for (int i = 0; i < n - 1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (v[j] > v[j + 1])
+            {
+                swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
+
+int partition(vector<int> &v, int low, int high)
+{
+    int pivot = v[high]; // lay phan tu cuoi lam pivot
+    int i = low - 1;
+    for (int j = low; j < high; j++)
+    {
+        if (v[j] < pivot)
+        {
+            i++;
+            swap(v[i], v[j]);
+        }
+    }
+    swap(v[i + 1], v[high]);
+    return i + 1;
+}
+
+// Ham quick sort de quy
+void quickSort(vector<int> &v, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(v, low, high);
+        quickSort(v, low, pi - 1);
+        quickSort(v, pi + 1, high);
+    }
+}
+
+vector<int> randomSorted(const vector<int> &v)
+{
+    vector<int> result = v; // Sao chep vector goc
+
+    int n = result.size();
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, n - 1);
+    for (int i = 0; i < n / 5; ++i)
+    {
+        int idx1 = dis(gen);
+        int idx2 = dis(gen);
+        if (idx1 != idx2)
+        {
+            swap(result[idx1], result[idx2]);
+        }
+    }
+
+    return result;
 }
 
 int main()
